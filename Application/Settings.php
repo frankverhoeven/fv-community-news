df3670cd08ad61e2bd6db1a1d6a08121bc47faf5
frankverhoeven <frankverhoeven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-20 21:19:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-community-news/trunk@264901 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Application/Settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-class fvCommunityNewsSettings extends fvCommunityNewsSettings_Abstract {
-	
-	protected $_prefix = 'fvcn_';
-	
-}
-
--- Application/View/Widget/FormSettings.php
+++
@@ -1,8 +0,0 @@
-<p>
-	<label for="<?php echo $this->wpWidget->get_field_id('fvcn_FormTitle'); ?>"><?php _e('Title', 'fvcn'); ?></label>
-	<input type="text" id="<?php echo $this->wpWidget->get_field_id('fvcn_FormTitle'); ?>" name="<?php echo $this->wpWidget->get_field_name('fvcn_FormTitle'); ?>" value="<?php echo esc_attr($this->instance['fvcn_FormTitle']); ?>" class="widefat" />
-</p>
-<p>
-	<label for="<?php echo $this->wpWidget->get_field_id('fvcn_FormDescription'); ?>"><?php _e('Description', 'fvcn'); ?></label>
-	<textarea id="<?php echo $this->wpWidget->get_field_id('fvcn_FormDescription'); ?>" name="<?php echo $this->wpWidget->get_field_name('fvcn_FormDescription'); ?>" rows="7" cols="20" class="widefat"><?php echo $this->instance['fvcn_FormDescription']; ?></textarea>
-</p>
