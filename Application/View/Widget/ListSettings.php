2295af55aa093fff73410ac98b60ddfcee6f980c
frankverhoeven <frankverhoeven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 00:54:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-community-news/trunk@264976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Application/View/Widget/ListSettings.php
+++
@@ -1,4 +0,0 @@
-<p>
-	<label for="<?php echo $this->wpWidget->get_field_id('fvcn_SubmissionsTitle'); ?>"><?php _e('Title', 'fvcn'); ?></label>
-	<input type="text" id="<?php echo $this->wpWidget->get_field_id('fvcn_SubmissionsTitle'); ?>" name="<?php echo $this->wpWidget->get_field_name('fvcn_SubmissionsTitle'); ?>" value="<?php echo esc_attr($this->instance['fvcn_SubmissionsTitle']); ?>" class="widefat" />
-</p>
