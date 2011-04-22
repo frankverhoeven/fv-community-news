5ab9e922ec375cc53e22ed4cc0b15529a086dc95
frankverhoeven <frankverhoeven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-22 12:49:38 +0000

    Deleting v2.0-alpha1
    git-svn-id: https://plugins.svn.wordpress.org/fv-community-news/trunk@375934 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Application/View/Widget/Form.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo $this->args['before_widget'];
-
-echo $this->args['before_title'] . $this->title . $this->args['after_title'];
-
-echo $this->description;
-$this->form->render();
-
-echo $this->args['after_widget'];
