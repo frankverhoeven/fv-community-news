2747aa3a8f215efea6aabba82aa800463a36f894
frankverhoeven <frankverhoeven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-24 09:01:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fv-community-news/trunk@376504 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Includes/Admin/Template/dashboard-settings.php
+++
@@ -1,5 +0,0 @@
-<p>
-	<label for="fvcn_NumDashboardListItems"><?php _e('Number of submissions to show:'); ?></label>
-	<input type="text" name="fvcn_NumDashboardListItems" id="fvcn_NumDashboardListItems" value="<?php echo fvcn_get_setting('fvcn_NumDashboardListItems'); ?>" size="3" />
-	<small><?php _e('(at most 30)', 'fvcn'); ?></small>
-</p>
